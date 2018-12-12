60fbc832ed84b38c94d30efcb76516275be21cb1
Mateusz <matijurkiewicz352@gmail.com>
2018-12-12 16:56:58 +0100

    SCI-Ge

--- source/sci/main.cpp
+++ source/sci/main.cpp
@@ -1,10 +1,35 @@
 #include <cstdlib>
 #include <sci/Application.hpp>
+#include <SDL2/sdl.h>
+#include <iostream>
 
+using namespace std;
+
+int status = 0;
 
 int main(int argc, char* argv[])
 {
     Application::create_instance();
+	SDL_Window*window=SDL_CreateWindow("SCIokno1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);     
+	 
+	while (status == 0)
+	{
+		SDL_Event zdarzenie;
+		while (SDL_PollEvent(&zdarzenie))
+		{
+			switch(zdarzenie.type)
+				case SDL_KEYDOWN:
+				case SDL_KEYUP:
+					cout << zdarzenie.key.timestamp;
+					cout << " ";
+					cout << zdarzenie.key.keysym.scancode;
+					break;
+
+
+			
+		}
+	}
+
 
     return 0;
 }
